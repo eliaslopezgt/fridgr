e50f77b732d8c18241b61eb19b1feb86c92da437
Elias Lopez Gutierrez <eliaslopezgtz@gmail.com>
2015-05-28 21:49:46 -0500

    change item service to container service

---
+++ app/services/container_service.rb
@@ -0,0 +1,6 @@
+class ItemService
+  def self.store_item(item_to_store)
+    p item_to_store
+    binding.pry
+  end
+end
--- app/services/item_service.rb
+++ app/services/item_service.rb
@@ -1,5 +1,11 @@
-class ItemService
-  def self.store_item(item_to_store)
+class ContainerService
+  
+  def initialize(container)
+    @container = container
+  end
+
+  def store_item(item_to_store)
     p item_to_store
+    binding.pry
   end
 end
