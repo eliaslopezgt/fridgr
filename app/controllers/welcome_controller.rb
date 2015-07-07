c0b661f23a6bb8d2af70803d8dbdc81c6439b2a1
Elias Lopez Gutierrez <elias.lopez@senioradvisor.com>
2015-07-06 19:29:57 -0500

    test

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,4 @@
 class WelcomeController < ApplicationController
-  caches_action :index, :expires_in => 24.hours
   def index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
   get 'welcome/index'
-  resources :invitaciones
+  post '/invitaciones' => 'invitaciones#create', :as => :invitaciones_create
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
   # mount using a custom path
-  #post '/invitaciones' => 'invitaciones#create', :as => :invitaciones_create
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
