153242e829d4fb03b853f17e8fbab2d0c75a12c4
Elias Lopez Gutierrez <elias.lopez@senioradvisor.com>
2015-07-07 22:37:02 -0500

    more changeS

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def render_esi(path)
+    if Rails.env.development?
+      div_id = Digest::MD5.hexdigest(path + rand.to_s)
+      script = "$.ajax({" +
+          "type:\"GET\"," +
+          "async: false," +
+          "url:\"#{path}\"," +
+          "dataType:\"html\"," +
+          "success:function(html) {$(\"##{div_id}\").html(html)}});"
+      content_tag(:div, '', :id => div_id) + content_tag(:script, script.html_safe, :type => 'text/javascript')
+    else
+      "<esi:include src=\"#{path}\" >>"
+    end
+  end
 end
